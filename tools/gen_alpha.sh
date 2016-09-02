24c8fe16886e846945e47ac5dc2a33cf5412d68e
dgengtek <dgengtek+github@gmail.com>
2016-09-02 17:11:27 +0200

    Deleting unused scripts

--- tools/gen_alpha.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-length=$1
-if [ -z "$length" ];then
-  length=8
-fi
-tr -cd [:alnum:] < /dev/urandom | head -c "$length" | xargs -0
--- tools/gen_pw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-length=$1
-if [ -z "$length" ];then
-  length=8
-fi
-tr -cd [:graph:] < /dev/urandom | head -c "$length" | xargs -0
