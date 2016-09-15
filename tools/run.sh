209e3220c8330890dabfe01cb769869945fcb05f
dgengtek <dgengtek+github@gmail.com>
2016-09-15 23:53:43 +0200

    Rewrite run script to use notifications

--- tools/run.sh
+++ tools/run.sh
@@ -1,20 +1,88 @@
 #!/bin/env bash
 usage() {
-    echo "no arguments to start a command in background" >&2
-    exit 1
+  cat >&2 << EOF
+Usage: ${0##*/} [OPTIONS] CMD...
+
+Run commands in background
+
+OPTIONS:
+  -l, --log         log command output
+  -s, --silent      disable notifications   
+  -h                help
+EOF
 }
 main() {
-  if [ -z "$1" ]; then
+  cmd=("$@")
+  local -i enable_verbose=0
+  local -i enable_loggin=0
+  local -i enable_notifications=1
+
+  local logfile="/dev/null"
+  local commands=
+  parse_options "$@"
+  set -- ${commands[@]}
+  unset -v commands
+
+  if [[ $# < 1 ]]; then
     usage
+    error_exit 1 "No commands."
   fi
 
-  cmd=("$@")
-  cmdstring=$(echo "$cmd" | cut -f1 -d ' ')
+  #trap handle_signal SIGINT SIGTERM SIGKILL EXIT
 
-  logfile="/dev/null"
+  if (($enable_loggin)); then
+    logfile="log_run.out"
+    echo "$@" > "$logfile"
+  fi
 
-  $("${cmd[@]}" &> $logfile) &
-  cmd_pid=$!
+  exec 1>>"$logfile"
+  exec 2>&1
+  run_commands "$@" &
+}
+run_commands() {
+  eval "$@"
+  if (($enable_notifications)); then
+    mplayer "$BEEP"
+    local -r message=$@
+    notify-send "Background process done" "'$message'"
+  fi
+}
+parse_options() {
+  # exit if no options left
+  [[ -z $1 ]] && return 0
+
+  local do_shift=0
+  case $1 in
+      -l|--log)
+        enable_loggin=1
+        ;;
+      -s|--silent)
+        enable_notifications=0
+        ;;
+      *)
+        do_shift=1
+	;;
+  esac
+  if (($do_shift)) ; then
+    commands+=("$1")
+  fi
+  shift
+  parse_options "$@"
+}
+log() {
+  echo -n "$@" | logger -s -t ${0##*/}
+}
+error_exit() {
+  error_code=${1:-0}
+  shift
+  log "$@"
+  exit $error_code
+}
+
+handle_signal() {
+  trap - SIGINT SIGTERM SIGKILL EXIT
+  kill $$
+  error_exit 1 "Signal received: stopping backup" 
 }
 
 main "$@"
