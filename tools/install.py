aeeed0a17d1c3a621f6747a39d08ea6ea9d6eea2
dgengtek <dgengtek@users.noreply.github.com>
2016-10-03 19:25:06 +0200

    Use stow wrapper with configuration files

--- tools/install.py
+++ tools/install.py
@@ -1,7 +1,5 @@
 #!/bin/env python3
-# outdated use python pystow app
 """
-Requires stow_dir_setup.sh to run
 used to parse configuration for deployment of directory structure
 
 """
@@ -11,6 +9,7 @@ import sys
 
 # TODO improve logging
 import logging
+from pathlib import Path
 
 # TODO use glob(from pathlib with Path("path/to/dir").glob("ew*"), and pathlib
 # for linux and nt
@@ -24,19 +23,20 @@ _keys = [
   "pkgs",
   "exclude"
   ]
-_configuration_filenames = [
-        "pystow.cfg",
-        ]
 _logger = logging.getLogger(sys.argv[0])
 _logger.setLevel(logging.INFO)
 _ch = logging.StreamHandler()
 _formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 _ch.setFormatter(_formatter)
 _logger.addHandler(_ch)
+_cmd = "stow"
 # default config filename
-_config_filename = "setup.ini"
+_config_filename = { "setup.ini", }
 
 def main():
+    global _cmd
+    _cmd = str(get_cmd_path(_cmd))
+
     # keys for stow setup in sections
     parser = ConfigParser()
 
@@ -44,31 +44,28 @@ def main():
     main.config=""
 
     input_args = parse_argv(sys.argv[1:])
-    main.config = input_args.get("file",_config_filename)
-    if not main.config:
-        main.config = _config_filename
-    if not os.path.isfile(main.config):
+    main.config = input_args.get("config",_config_filename)
+    main.config = Path(main.config)
+    if not main.config.is_file():
         print("No configuration file found")
         usage()
-    if not parser.read(main.config):
+
+    if not parser.read(str(main.config)):
         printexit("Configuration: {} not found.".format(config))
-    main.config = os.path.abspath(main.config)
 
     sections = parser.sections()
-    cmd = get_cmd_path("stow_dir_setup.sh")
-
     for section in sections:
         values = get_values(parser[section],_keys)
-        path = build_path(values.get("path"))
+        path = Path(values.get("path"))
         values.update({"path":path})
         args = build_args(values, **input_args)
-        run(cmd, args)
+        run(_cmd, args)
+    os.wait()
 
 def usage():
-    hilfe="usage:  "+sys.argv[0]+" ini"
-    cmd = get_cmd_path("stow_dir_setup.sh")
+    global _cmd
+    cmd = get_cmd_path(_cmd)
     os.execv(cmd,["-h"])
-    print(hilfe)
     sys.exit(1)
 
 def parse_argv(argv):
@@ -80,39 +77,32 @@ def parse_argv(argv):
 
 def get_argparser():
     summary="""
-    Pystow
+    stow installer wrap
 
     """
     argparser = ArgumentParser(description=summary)
 
-    argparser.add_argument("-v","--verbose",action="store_true",
-            help="")
-    argparser.add_argument("-d","--destination",
-            help="")
-    argparser.add_argument("-f","--file",
-            help="Configuration file")
-    argparser.add_argument("-s","--simulate",action="store_true", 
-            help="Simulate")
-    argparser.add_argument("-t","--target", 
-            help="Target")
+    argparser.add_argument("config", help="Ini configuration file")
     return argparser
 
 def get_cmd_path(cmd):
     # if on local path return 
+    cmd = Path(cmd)
     if cmd_exists(cmd):
         return cmd
     # try in tools
-    elif cmd_exists("tools/"+cmd):
-        return "tools/"+cmd
+    elif cmd_exists(Path("tools", cmd)):
+        return Path("tools/",cmd)
 
     path = os.environ["PATH"].split(os.pathsep)
     for p in path:
-        ex = p + "/" + cmd
-        if cmd_exists(ex):
+        ex = Path(p ,cmd)
+        if ex.is_file():
           return ex
-    printexit("stow wrapper not found in env PATH")
+    printexit("{} not found in PATH".format(cmd))
 
 def cmd_exists(cmd):
+    cmd = str(cmd)
     return os.path.isfile(cmd) and os.access(cmd, os.X_OK)
 
 def printerr(string):
@@ -126,44 +116,9 @@ def get_values(section, keys):
     d=dict()
     for key in keys:
         value = section.get(key)
-        #if not value:
-        #  printexit("Empty value for key:{} in section:{}".format(key,section))
         d.update({key:value})
     return d
 
-def build_path(path, check_valid=True):
-    parse_functions = [
-            parse_home,
-            parse_real,
-        ]
-    if check_valid:
-        parse_functions.append(path_check)
-
-    for function in parse_functions:
-        result = function(path)
-        if result:
-            path = result
-    return path
-
-def path_check(path):
-    _logger.info("Check path: {}".format(path))
-    if not path_valid(path):
-        raise NotADirectoryError("Path: {} is invalid".format(path))
-
-def path_valid(path):
-    return os.path.isdir(path) and os.access(path, os.X_OK)
-
-def parse_home(path):
-    if "~" not in path:
-        return 
-    pos = path.find("~")
-    suffix = path[pos+1:]
-    return os.getenv("HOME") + suffix
-
-def parse_real(path):
-    if path.startswith("/"):
-        return
-    return os.path.realpath(path)
 
 def filter_packages(pkgs=[], exclude=[]):
     if len(pkgs) is 0:
@@ -172,44 +127,61 @@ def filter_packages(pkgs=[], exclude=[]):
         if p not in exclude:
             yield p
 
+def _get_absolute_path(path):
+    path = path.expanduser()
+    path = path.absolute()
+    while not path.is_dir():
+        path = path.parent
+    return str(path)
 
 def build_args(values, filter_pkgs=filter_packages, **kwargs):
-    path = values.get("path")
+    global _cmd
 
-    source = os.getcwd()
+    path = values.get("path")
+    path = _get_absolute_path(path)
+
+    # change directory to root location of config file
+    source = main.config
+    source = _get_absolute_path(source)
+    os.chdir(source)
+
     args = list()
-    # $0
-    args.append(sys.argv[0])
-    # $1 ...
-    args.append("-d " + source)
-    args.append("-t " + path)
-    for key, value in kwargs.items():
-        if key == "simulate":
-            args.append("-s")
+    args.append(_cmd)
+    #args.extend(["-d", source])
+    args.extend(["-t", path])
+
+    # get all packages
     pkgs = values.get("pkgs", "")
     if not pkgs or pkgs.startswith("*"):
       pkgs = os.listdir(source)
     else:
       pkgs = pkgs.split(",")
 
-    excluded_packages = values.get("exclude", "")
+    # filter excluded
+    excluded_packages = values.get("exclude", [])
     if excluded_packages:
         excluded_packages = excluded_packages.split(",")
     else:
         excluded_packages = list()
+
+    # always filter git
     excluded_packages.append(".git")
+
     pkgs = filter_pkgs(pkgs, excluded_packages)
 
     for p in pkgs:
+        # only stow directories
         if not os.path.isdir(p):
           continue
-        args.append("-p "+p)
+        args.extend(["-p", p])
     return args
 
 def run(cmd, args):
+    if isinstance(cmd, Path):
+        cmd = str(cmd)
     pid = os.fork()
     if pid == 0:
-        os.execv(cmd,args)
+        os.execv(cmd ,args)
 
 
 if __name__ == "__main__":
