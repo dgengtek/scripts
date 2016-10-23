4bc761435d4b11f2bbfb6ad38356fdea5d26e6a9
dgengtek <dgengtek@users.noreply.github.com>
2016-10-23 18:27:03 +0200

    Add custom prompt to ssh session

--- tools/sshsession.py
+++ tools/sshsession.py
@@ -15,6 +15,10 @@ from docopt import docopt
 # TODO check ssh path exists
 # TODO add logging
 # TODO add unit tests
+# TODO add bash prompt coloring
+#       research: import PS1 from current invoked interactive bash session?
+
+used_keys = []
 
 def main():
     # parse docopt
@@ -30,21 +34,58 @@ def main():
     else:
         success = run_non_interactive(main.ssh_path, ssh_keys)
     if success:
-        os.execl("/usr/bin/bash","bash", "-i")
+        print()
+        run_sshsession()
+
     else:
         print_error("Failed sshsession.")
         sys.exit(1)
 
+def run_sshsession(custom_prompt=True):
+    import tempfile
+    global used_keys
+
+    with tempfile.NamedTemporaryFile(delete=True) as f:
+        prompt = ""
+        if custom_prompt:
+            COLOR = "\033[0;33m"
+            COLOR_NONE = "\033[0m"
+            prompt= r"""
+PS1="({1}SSHSESSION{2} {0})
+$(__set_custom_bash_prompt "$?" "\u" "@\h#\W]$ ")"
+""".format(used_keys, COLOR, COLOR_NONE)
+        bash_cmd = r"""
+source ~/.bashrc
+{0}
+        """.format(prompt)
+        with open(f.name, "w") as fo:
+            fo.write(bash_cmd)
+
+        command = [
+                "/usr/bin/bash",
+                "--rcfile",
+                f.name,
+                "-i",
+                ]
+        subprocess.call(command) 
+        #os.execl("/usr/bin/bash","bash", "-i")
+
+
 def run_non_interactive(path, ssh_keys):
+    global used_keys
     added_once = False
+
     for ssh_key in ssh_keys:
         key_path = path.joinpath(ssh_key)
+        used_keys.append(os.basename(str(key_path)))
         proc = add_ssh_key(key_path)
         if not added_once and proc.returncode is 0:
             added_once = True
     return added_once
 
 def run_interactive(path):
+    global used_keys
+
     glob = r"id*.pub"
     ids = path.glob(glob)
     ids = list(ids)
@@ -56,7 +97,9 @@ def run_interactive(path):
         print(menu)
         choice = interactive_input(ids)
         if choice is not None:
-            proc = add_ssh_key(path.joinpath(ids[choice]))
+            key = ids[choice]
+            used_keys.append(os.path.basename(str(key)))
+            proc = add_ssh_key(path.joinpath(key))
             if proc.returncode is 0:
                 return True
             else:
