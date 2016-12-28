d51aa5edd2f15e42ad20b2020e014c8d67f0c3ff
dgengtek <dgengtek@users.noreply.github.com>
2016-12-28 15:41:37 +0100

    Fix char padding function

--- lib/libutils.sh
+++ lib/libutils.sh
@@ -1,19 +1,12 @@
 #!/bin/env bash
-print_chars() {
+pad_line() {
   usage=$(cat << EOF
-usage $0 char count
+Usage: $FUNCNAME [count]
   default count of 10
 EOF
   )
-  if [[ -z $1 ]]; then
-    echo "$usage"
-    exit 1
-  fi
-  local -i count=10
-  if [[ -n $2 ]]; then
-    count=$2
-  fi
-  printf '=%.0s' {1..$count}
+  local -i count=${2:-10}
+  printf '#%.0s' {1..$count}
 }
 ord() { printf "%d" "'$1"; }
 chr() { printf \\$(printf '%03o' $1); }
