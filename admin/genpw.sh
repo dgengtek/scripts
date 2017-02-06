634fb48d7faecf6393b8f0fa7c5d5b1e2bdd44ab
dgengtek <dgengtek@users.noreply.github.com>
2017-02-06 22:23:47 +0100

    Generate a table of passwords

--- admin/genpw.sh
+++ admin/genpw.sh
@@ -9,11 +9,13 @@ generate password
 options:
   -a            alphanumeric
   -g            (default)all printable characters, excluding space
+  -p            pin
+
 EOF
 }
 
 main() {
-  local -r optlist=":ag"
+  local -r optlist=":agp"
   local charset="graph"
   while getopts $optlist opt; do
     case $opt in
@@ -23,6 +25,9 @@ main() {
       g)
         charset="graph"
 	;;
+      p)
+        charset="digit"
+	;;
       *)
         usage
         exit 1
@@ -31,7 +36,26 @@ main() {
   done
   shift $(($OPTIND - 1))
   length=${1:-8}
-  tr -cd [:"$charset":] < /dev/urandom | head -c "$length" | xargs -0
+
+  display $length
+}
+display() {
+  local length=${1:?"Length not set."}
+  local columns=${2:-6}
+  local rows=${3:-10}
+  for i in $(seq $rows); do
+    for j in $(seq $columns); do
+      genpw $length
+    done
+    echo
+  done
+
+}
+genpw() {
+  local length=${1:?"Length not set."}
+  pw=$(tr -cd [:"$charset":] < /dev/urandom | head -c "$length" | xargs -0)
+  printf "%b" "$pw"
+  printf "    "
 }
 
 main "$@"
