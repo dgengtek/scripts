4eb07315eca36bfa7b1899eb8bf1e7baab765731
dgengtek <dgengtek@users.noreply.github.com>
2017-04-23 15:26:49 +0200

    Update encryption
    use stdin if possible
    fix adding salt

--- crypto/encrypt.py
+++ crypto/encrypt.py
@@ -2,7 +2,7 @@
 """
 Encrypt password with salt for unix
 Usage:
-    encrypt.py [options] [--sha512 | --sha256 | --md5 | --crypt] [salt] <password>
+    encrypt.py [options] [--sha512 | --sha256 | --md5 | --crypt] [<salt>]
 
 Options:
     --sha512
@@ -13,6 +13,7 @@ Options:
 
 import sys
 import crypt
+from getpass import getpass
 from docopt import docopt
 
 # docopt(doc, argv=None, help=True, version=None, options_first=False))
@@ -38,8 +39,17 @@ def main():
 
     method = get_method(opt)
 
-    salt = crypt.mksalt(method)
-    password = opt.get("<password>")
+    salt = opt.get("<salt>")
+    if not salt:
+        salt = crypt.mksalt(method)
+
+    password = ""
+    if not sys.stdin.isatty():
+        password = sys.stdin.readline()
+    else:
+        password = getpass()
+    if not password:
+        sys.exit(1)
 
     shadow = crypt.crypt(password, salt)
     print(shadow)
