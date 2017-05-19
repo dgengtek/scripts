d5fcf6a530c0bd74a7b1c368b19edcf8c9466e4a
dgengtek <dgengtek@users.noreply.github.com>
2017-05-19 23:47:07 +0200

    Update bash logging

--- lib/libutils.sh
+++ lib/libutils.sh
@@ -8,6 +8,7 @@ EOF
   local -i count=${2:-10}
   printf '#%.0s' {1..$count}
 }
+
 ord() { printf "%d" "'$1"; }
 chr() { printf \\$(printf '%03o' $1); }
 
@@ -25,16 +26,21 @@ echo_e() (
   IFS=" "
   printf '%b\n' "$*"
 )
+
+log() { echo "$@" | logger -t ${0##*/}; }
+loge() { echo "$@" | logger -s -t ${0##*/}; }
 out() { echo "$1 $2" "${@:3}"; }
-error() { out "==> ERROR:" "$@"; } >&2
-msg() { out "==>" "$@"; }
-msg2() { out "  ->" "$@";}
-ignore_error() { log "$@" 2>/dev/null; }
-log() { echo "$@" | logger -s -t ${0##*/}; }
+
+error() { loge "==> ERROR:" "$@"; } >&2
+ignore_error() { loge "$@" 2>/dev/null; }
+
+msg() { log "==>" "$@"; }
+msg2() { log "  ->" "$@";}
+
 error_exit() {
   error_code=$1
   shift
-  log "$@"
+  loge "$@"
   exit $error_code
 }
 die() { error_exit 1 "$@"; }
