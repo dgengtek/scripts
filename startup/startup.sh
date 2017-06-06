0a81374531c6ae787e13725c81a5c27af672af21
dgengtek <dgengtek@users.noreply.github.com>
2017-06-06 22:21:41 +0200

    Add dev startup

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 exec 2>&1
 exec 1>/dev/null
-firefox &
-keepassx &
-urxvt -e bash --init-file <(cat ~/.bashrc;echo "tmuxp load wiki.yaml") &
+run.sh firefox
+run.sh keepassx 
+run.sh urxvt -e bash --init-file <(cat ~/.bashrc;echo "tmuxp load wiki.yaml")
