24e0c41164209e96122bb67a90a426d3df9545d2
dgengtek <dgengtek@users.noreply.github.com>
2017-06-11 23:18:03 +0200

    Fix startup script

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,4 +3,4 @@ exec 2>&1
 exec 1>/dev/null
 run.sh firefox
 run.sh keepassx 
-run.sh urxvt -e bash --init-file <(cat ~/.bashrc;echo "tmuxp load wiki.yaml")
+run.sh urxvt -e bash --init-file <(cat ~/.bashrc;echo "tmuxp_start wiki.yaml")
