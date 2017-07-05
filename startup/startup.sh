5c2a17c1bfa66b61e55318b3e5f49191f865004f
dgengtek <dgengtek@users.noreply.github.com>
2017-07-05 15:29:34 +0200

    Add agent terminal

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,4 +3,5 @@ exec 2>&1
 exec 1>/dev/null
 run.sh firefox
 run.sh keepassx 
-run.sh urxvt -e bash --init-file <(cat ~/.bashrc;echo "tmuxp_start wiki.yaml")
+run.sh urxvtc -e tmux new ssh-agent sshsession.py
+run.sh urxvtc -e bash --init-file <(cat ~/.bashrc;echo "tmuxp_start wiki.yaml")
