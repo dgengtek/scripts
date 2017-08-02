5e4a9304ba24f6996a3aeaccc776ff53347e0c5a
dgengtek <dgengtek@users.noreply.github.com>
2017-08-02 13:42:29 +0200

    Fix error handling during execution of countdown

--- tools/countdown.py
+++ tools/countdown.py
@@ -6,72 +6,6 @@ Usage:
 Options:
 """
 
-"""
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
-Usage:
-    my_program tcp <host> <port> [--timeout=<seconds>]
-    my_program serial <port> [--baud=<n>] [--timeout=<seconds>]
-    my_program (-i | --interactive)
-    my_program (-h | --help | --version)
-Options:
-    -i, --interactive  Interactive Mode
-    -h, --help  Show this screen and exit.
-    --baud=<n>  Baudrate [default: 9600]
-<arguements>, ARGUMENTS -> list
---options, words with dash(-) , or --input=FILE or -i FILE
-commands, dont follow above
-
-Pattern constructs:
-    [](brackets) optional elements
-    ()(parentheses) required elements, everything not put in [] also required
-    |(pipe) mutually exclusive elements. Group with () or []
-    ...(ellipsis) one or more elements, e.g. my_program.py FILE ..., one ore
-        more FILE s accepted, for zero or more use [FILE ...]
-    [options](case sensitive) shortcut for options, defined in options below
-    "[--]" used by convetntion to separate positional arguements
-    "[-]" by conevntion signify stdin is used instead of a file
-    [-v | -vv | -vv] countable flags, args["-v"] will be nr of occ
-
-    Options:
-      --verbose   # GOOD
-      -o FILE     # GOOD
-    Other: --bad  # BAD, line does not start with dash "-"
-
-    -o FILE --output=FILE       # without comma, with "=" sign
-    -i <file>, --input <file>   # with comma, without "=" sing
-
-    Use two spaces to separate options with their informal description
-	--verbose More text.   # BAD, will be treated as if verbose option had
-			       # an argument "More", so use 2 spaces instead
-	-q        Quit.        # GOOD
-	-o FILE   Output file. # GOOD
-	--stdout  Use stdout.  # GOOD, 2 spaces
-
-    If you want to set a default value for an option with an argument, 
-    put it into the option-description, in form [default: <my-default-value>]:
-	--coefficient=K  The K coefficient [default: 2.95]
-	--output=FILE    Output file [default: test.txt]
-	--directory=DIR  Some directory [default: ./]
-
-    for git like sub commands use, options_first parameter 
-
-     args = docopt(__doc__,
-                  version='git version 1.7.4.4',
-                  options_first=True)
-    print('global arguments:')
-    print(args)
-    print('command arguments:')
-
-    argv = [args['<command>']] + args['<args>']
-    if args['<command>'] == 'add':
-        # In case subcommand is implemented as python module:
-        import git_add
-    print(docopt(git_add.__doc__, argv=argv))
-
-"""
-
-
 import sys
 from docopt import docopt
 import logging
@@ -279,11 +213,21 @@ def main():
     #print(opt)
     seconds = opt.get("<seconds>")
     counter = None
-    if seconds:
+    if not seconds:
+        print(__doc__)
+        sys.exit(1)
+
+    try:
         timer = Countdown.from_seconds(int(seconds))
         counter = Counter(timer)
         counter()
         counter.wait()
+    except Exception as e:
+        logger.error(e)
+        counter.stop()
+    except KeyboardInterrupt:
+        print("\nbye", file=sys.stderr, end="")
+        counter.stop()
 
 @pytest.mark.parametrize("seconds, expected", [
             (1, (0,0,1)),
