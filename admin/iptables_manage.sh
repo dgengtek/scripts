4e2dbbcd3a49aba83806927766aeb6c90588b60a
dgengtek <dgengtek@users.noreply.github.com>
2017-08-16 21:34:37 +0200

    Update iptables management
    * add panic mode
    * split rules to functions

--- admin/iptables_manage.sh
+++ admin/iptables_manage.sh
@@ -3,11 +3,6 @@
 # description
 # ------------------------------------------------------------------------------
 # 
-# add following to /etc/sysctl.conf
-#
-# net.ipv4.conf.all.rp_filter=1
-# net.ipv4.conf.all.log_martians=1
-# net.ipv4.conf.default.log_martians=1
 # TODO add iptables.rules file for temporary storage of all rules
 # TODO append all rules to file and use at the end iptables restore to active
 # TODO allow debug mode where no rules are applied only sent to output
@@ -19,14 +14,9 @@ OPTIONS:
   -h			  help
   -v			  verbose
   -f, --flush		  flush
+  -p, --panic             panic mode, only ssh
   -q			  quiet
   -d			  debug
-
-arg1
-  mandatory argument passed to script
-
-EXTRA
-  Additional options passed for other purposes
 EOF
 }
 
@@ -35,7 +25,11 @@ main() {
   local -r SERVER_IP="202.54.10.20" # server IP
   local -r LAN_RANGE="192.168.1.0/24" # your LAN IP range 
   local -r IPTABLES_BIN="/usr/bin/iptables" # path to iptables
+
+  local -r sysctl_file="90-firewall.conf"
+
   # Add your spoofed IP range/IPs here
+  # RFC 1918
   SPOOF_IPS=(
   "0.0.0.0/8"
   "127.0.0.0/8"
@@ -47,6 +41,7 @@ main() {
    
   # default action, can be DROP or REJECT 
   ACTION="DROP"
+  DEFAULT_CHAIN_ACTION="DROP"
 
 
   # flags
@@ -70,22 +65,28 @@ main() {
   setup
   run
 }
+default_CHAINS() {
+  $IPTABLES_BIN -N TCP
+  $IPTABLES_BIN -N UDP
 
-defaults() {
-  # Reject spoofed packets
-  $IPTABLES_BIN -A INPUT -s 10.0.0.0/8 -j DROP
-  $IPTABLES_BIN -A INPUT -s 169.254.0.0/16 -j DROP
-  $IPTABLES_BIN -A INPUT -s 172.16.0.0/12 -j DROP
-  $IPTABLES_BIN -A INPUT -s 127.0.0.0/8 -j DROP
+}
+default_POLICY() {
+  $IPTABLES_BIN -P INPUT "$DEFAULT_CHAIN_ACTION"
+  $IPTABLES_BIN -P FORWARD "$DEFAULT_CHAIN_ACTION"
+  $IPTABLES_BIN -P OUTPUT ACCEPT
+}
 
-  $IPTABLES_BIN -A INPUT -s 224.0.0.0/4 -j DROP
-  $IPTABLES_BIN -A INPUT -d 224.0.0.0/4 -j DROP
-  $IPTABLES_BIN -A INPUT -s 240.0.0.0/5 -j DROP
-  $IPTABLES_BIN -A INPUT -d 240.0.0.0/5 -j DROP
-  $IPTABLES_BIN -A INPUT -s 0.0.0.0/8 -j DROP
-  $IPTABLES_BIN -A INPUT -d 0.0.0.0/8 -j DROP
-  $IPTABLES_BIN -A INPUT -d 239.255.255.0/24 -j DROP
-  $IPTABLES_BIN -A INPUT -d 255.255.255.255 -j DROP
+add_kernel_parameters() {
+  local -ar kernel_options=(
+  "net.ipv4.conf.all.log_martians=1"
+  "net.ipv4.conf.default.log_martians=1"
+  )
+  if [[ -f "/etc/sysctl.d/$sysctl_file" ]]; then
+    :
+  fi
+}
+
+default_BLOCKS() {
 
 # Stop smurf attacks
   $IPTABLES_BIN -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j DROP
@@ -115,7 +116,36 @@ defaults() {
 
   $IPTABLES_BIN -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "Portscan:"
   $IPTABLES_BIN -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
+}
+default_ACCEPT() {
+  $IPTABLES_BIN -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+  $IPTABLES_BIN -A INPUT -i lo -j ACCEPT
+  $IPTABLES_BIN -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+  $IPTABLES_BIN -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 
+  $IPTABLES_BIN -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+  $IPTABLES_BIN -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+
+
+  $IPTABLES_BIN -A TCP -p tcp --dport 80 -j ACCEPT
+  $IPTABLES_BIN -A TCP -p tcp --dport 443 -j ACCEPT
+  $IPTABLES_BIN -A TCP -p tcp --dport 22 -j ACCEPT
+  $IPTABLES_BIN -A TCP -p tcp --dport 53 -j ACCEPT
+  $IPTABLES_BIN -A UDP -p udp --dport 53 -j ACCEPT
+
+  $IPTABLES_BIN -t raw -I PREROUTING -m rpfilter --invert -j DROP
+
+# syn scans
+  $IPTABLES_BIN -I TCP -p tcp -m recent --update --seconds 60 --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+  $IPTABLES_BIN -D INPUT -p tcp -j REJECT --reject-with tcp-reset
+  $IPTABLES_BIN -A INPUT -p tcp -m recent --set --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+
+# udp scans
+  $IPTABLES_BIN -I UDP -p udp -m recent --update --seconds 60 --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+  $IPTABLES_BIN -D INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+  $IPTABLES_BIN -A INPUT -p udp -m recent --set --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+
+  $IPTABLES_BIN -A INPUT -j REJECT --reject-with icmp-proto-unreachable
 }
 
 check_dependencies() {
@@ -160,7 +190,11 @@ setup() {
 }
 
 run() {
-  :
+  flush_all
+  block_spoof
+  default_POLICY
+  default_CHAINS
+  default_BLOCKS
 }
 
 parse_options() {
@@ -183,6 +217,10 @@ parse_options() {
 	;;
       -v|--verbose)
 	enable_verbose=1
+	;;
+      -p|--panic)
+        panic
+        exit 0
 	;;
       -q|--quiet)
         enable_quiet=1
@@ -223,33 +261,56 @@ cleanup() {
 }
 
 block_spoof() {
-  # Drop packet that claiming from our own server on WAN port
-  $IPTABLES_BIN -A INPUT -i $INTERFACE_INC -s $SERVER_IP -j $ACTION
-  $IPTABLES_BIN -A OUTPUT -o $INTERFACE_INC -s $SERVER_IP -j $ACTION
-   
-  # Drop packet that claiming from our own internal LAN on WAN port
-  $IPTABLES_BIN -A INPUT -i $INTERFACE_INC -s $LAN_RANGE -j $ACTION
-  $IPTABLES_BIN -A OUTPUT -o $INTERFACE_INC -s $LAN_RANGE -j $ACTION
-   
-  ## Drop all spoofed 
-  for ip in $SPOOF_IPS; do
-    $IPTABLES_BIN -A INPUT -i $INTERFACE_INC -s $ip -j $ACTION
-    $IPTABLES_BIN -A OUTPUT -o $INTERFACE_INC -s $ip -j $ACTION
-  done
+  # TODO add to only wan interface
+  return 0
+  # Reject spoofed packets
+  $IPTABLES_BIN -A INPUT -s 10.0.0.0/8 -j DROP
+  $IPTABLES_BIN -A INPUT -s 169.254.0.0/16 -j DROP
+  $IPTABLES_BIN -A INPUT -s 172.16.0.0/12 -j DROP
+  $IPTABLES_BIN -A INPUT -s 127.0.0.0/8 -j DROP
+
+  $IPTABLES_BIN -A INPUT -s 224.0.0.0/4 -j DROP
+  $IPTABLES_BIN -A INPUT -d 224.0.0.0/4 -j DROP
+  $IPTABLES_BIN -A INPUT -s 240.0.0.0/4 -j DROP
+  $IPTABLES_BIN -A INPUT -d 240.0.0.0/4 -j DROP
+  $IPTABLES_BIN -A INPUT -s 0.0.0.0/8 -j DROP
+  $IPTABLES_BIN -A INPUT -d 0.0.0.0/8 -j DROP
+  $IPTABLES_BIN -A INPUT -d 239.255.255.0/24 -j DROP
+  $IPTABLES_BIN -A INPUT -d 255.255.255.255 -j DROP
 }
 
 flush_all() {
   $IPTABLES_BIN -P INPUT ACCEPT
   $IPTABLES_BIN -P FORWARD ACCEPT
   $IPTABLES_BIN -P OUTPUT ACCEPT
+
+  $IPTABLES -t nat -P PREROUTING ACCEPT
+  $IPTABLES -t nat -P POSTROUTING ACCEPT
+  $IPTABLES -t nat -P OUTPUT ACCEPT
+
+  $IPTABLES -t mangle -P PREROUTING ACCEPT
+  $IPTABLES -t mangle -P POSTROUTING ACCEPT
+  $IPTABLES -t mangle -P INPUT ACCEPT
+  $IPTABLES -t mangle -P OUTPUT ACCEPT
+  $IPTABLES -t mangle -P FORWARD ACCEPT
+
   $IPTABLES_BIN -F
-  $IPTABLES_BIN -X
   $IPTABLES_BIN -t nat -F
-  $IPTABLES_BIN -t nat -X
   $IPTABLES_BIN -t mangle -F
+  $IPTABLES_BIN -t raw -F
+
+  $IPTABLES_BIN -X
+  $IPTABLES_BIN -t nat -X
   $IPTABLES_BIN -t mangle -X
-  $IPTABLES_BIN iptables -t raw -F
   $IPTABLES_BIN -t raw -X
+}
+panic() {
+  flush_all
+  $IPTABLES_BIN -A INPUT -p tcp --dport 22 --syn -m conntrack --ctstate NEW -j ACCEPT
+  $IPTABLES_BIN -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+  $IPTABLES_BIN -P INPUT DROP
+  $IPTABLES_BIN -P FORWARD DROP
+  $IPTABLES_BIN -P OUTPUT DROP
 }
 
 prepare
