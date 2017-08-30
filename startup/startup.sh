5bff777b157c7525375effe910b382066a8604ac
dgengtek <dgengtek@users.noreply.github.com>
2017-08-30 16:12:02 +0200

    Do not use ssh-agent anymore
    use gpg-agent

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,5 +3,4 @@ exec 2>&1
 exec 1>/dev/null
 run.sh firefox
 run.sh keepassx 
-run.sh urxvtc -e tmux new ssh-agent sshsession.py
-run.sh urxvtc -e bash --init-file <(cat ~/.bashrc;echo "tmuxp load ~/.tmuxp/wiki.yaml")
+run.sh urxvtc -e 'bash --init-file <(cat ~/.bashrc;echo "tmuxp load ~/.tmuxp/wiki.yaml")'
