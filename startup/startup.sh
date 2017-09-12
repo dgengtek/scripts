c13eef3c95444e9cdefc81aedbd4c12f52c92517
dgengtek <dgengtek@users.noreply.github.com>
2017-09-12 23:23:19 +0200

    Update startup script

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,5 +2,6 @@
 exec 2>&1
 exec 1>/dev/null
 run.sh firefox
-run.sh keepassx 
-run.sh urxvtc -e 'bash --init-file <(cat ~/.bashrc;echo "tmuxp load ~/.tmuxp/wiki.yaml")'
+#run.sh keepassx 
+tmux new -d -s "mutt" mutt
+run.sh -q urxvtc -e 'tmuxp load ~/.tmuxp/wiki.yaml'
