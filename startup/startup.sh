9f6aea1d332c155f32e7b324961e31cfe3b77403
dgengtek <dgengtek@users.noreply.github.com>
2017-12-31 15:35:39 +0100

    startup: Add mutt,irc

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,5 +3,6 @@ exec 2>&1
 exec 1>/dev/null
 run.sh firefox
 #run.sh keepassx 
-tmux new -d -s "mutt"
 run.sh -q urxvtc -e 'tmuxp load ~/.tmuxp/wiki.yaml'
+run.sh -q urxvtc -e 'tmux new -s mutt'
+run.sh -q urxvtc -e 'tmuxp load ~/.tmuxp/irc.yaml'
