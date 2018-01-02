6fbfa11a6f13f8271687a83fc4de68f62d4a4501
dgengtek <dgengtek@users.noreply.github.com>
2018-01-02 13:15:34 +0100

    Fix encrpytion with custom salt
    * format string for crypt
    * add rounds option

--- crypto/encrypt.py
+++ crypto/encrypt.py
@@ -2,13 +2,14 @@
 """
 Encrypt password with salt for unix
 Usage:
-    encrypt.py [options] [--sha512 | --sha256 | --md5 | --crypt] [<salt>]
+    encrypt.py [options] [--rounds <count>] [--sha512 | --sha256 | --md5 | --crypt] [<salt>]
 
 Options:
     --sha512
     --sha256
     --md5
     --crypt
+    -r, --rounds <count>  rounds[default: 1000]
 """
 
 import sys
@@ -19,29 +20,48 @@ from docopt import docopt
 # docopt(doc, argv=None, help=True, version=None, options_first=False))
 
 
-default_flag = {"--sha512":True}
+default_flag = "--sha512"
 
 methods = {
-        "--sha512" : crypt.METHOD_SHA512,
-        "--sha256" : crypt.METHOD_SHA256,
-        "--md5" : crypt.METHOD_MD5,
-        "--crypt" : crypt.METHOD_CRYPT,
+        "--sha512" : {
+            "method": crypt.METHOD_SHA512,
+            "id": "6",
+            },
+        "--sha256" : {
+            "method": crypt.METHOD_SHA256,
+            "id": "5",
+            },
+        "--md5" : {
+            "method": crypt.METHOD_MD5,
+            "id": "1",
+            },
+        "--crypt" : {
+            "method": crypt.METHOD_CRYPT,
+            "id": "",
+            },
         }
 
-def get_method(opt):
+def get_method(opt, default=default_flag):
     for key in methods.keys():
         if opt.get(key, False):
             return methods.get(key)
+    return methods.get(default_flag)
 
 
 def main():
     opt = docopt(__doc__, sys.argv[1:])
 
-    method = get_method(opt)
+    rounds = opt.get("--rounds")
+    methods = get_method(opt)
+    method = methods.get("method")
+    id_prefix = methods.get("id")
+    
 
     salt = opt.get("<salt>")
     if not salt:
         salt = crypt.mksalt(method)
+    else:
+        salt = "${}$rounds={}${}$".format(id_prefix, rounds, salt)
 
     password = ""
     if not sys.stdin.isatty():
