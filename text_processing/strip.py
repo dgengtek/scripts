6ddb091dba31132335ede140c23f8a56472622fb
dgengtek <dgengtek@users.noreply.github.com>
2018-03-07 12:34:57 +0100

    Add python strip

---
+++ text_processing/strip.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+import sys
+data_input = ""
+if len(sys.argv) == 1:
+    data_input = sys.stdin.readlines()
+else:
+    data_input = sys.argv[1:]
+
+data = " ".join(data_input)
+print(data .strip(), end='')
