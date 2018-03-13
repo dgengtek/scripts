10c70f1f6cc4ac794e6f127a933f4395ac922de1
dgengtek <dgengtek@users.noreply.github.com>
2018-03-13 16:07:48 +0100

    Use python -m json.tool instead

--- output/pprint_json.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python3
-import json
-import pprint
-import sys
-json_string = ""
-for line in sys.stdin.readlines():
-    json_string = "{}{}".format(json_string, line.strip())
-item = json.loads(json_string)
-pprint.pprint(item)
