4a21596c8b2f892263c8a19353b08136557de719
dgengtek <dgengtek@users.noreply.github.com>
2018-03-18 15:22:41 +0100

    Cleanup run

--- tools/run.sh
+++ tools/run.sh
@@ -7,6 +7,9 @@ Usage: ${0##*/} [OPTIONS] CMD...
 Run commands in background
 
 OPTIONS:
+  -d, --debug
+  -v, --verbose 
+  -q, --quiet
   -l, --log           log command output
   -m, --mail          mail result
   -o, --output        mail cmd output
@@ -14,12 +17,10 @@ OPTIONS:
   -f, --foreground    run in foreground
   -p, --print-process print process regardless
   -s, --sudo          run with sudo
-  -q, --quiet  quiet run
   -h                  help
 EOF
 }
 main() {
-  cmd=("$@")
   local -i enable_logging=0
   local -i enable_mail=0
   local -i enable_mail_cmd_output=0
@@ -27,6 +28,11 @@ main() {
   local -i enable_notifications=1
   local -i run_as_sudo=0
   local -i print_process=0
+
+  # flags
+  local -i enable_verbose=0
+  local -i enable_quiet=0
+  local -i enable_debug=0
 
   # mail recipient
   local recipient="notification"
@@ -42,9 +48,14 @@ main() {
     error_exit 1 "No commands."
   fi
 
-  trap signal_handler SIGINT SIGTERM 
-  trap cleanup EXIT
+  prepare_env
+  set_signal_handlers
+  run "$@"
+  unset_signal_handlers
 
+}
+
+run() {
   if (($enable_logging)) || (($enable_mail)); then
     logfile=$(mktemp -u /tmp/runXXXXXX.log)
     echo -e "$@\n" > "$logfile"
@@ -71,9 +82,8 @@ main() {
     run_commands "$@" &
     (! (($enable_foreground)) || (($print_process)) ) && echo "$!" >&3
   fi
-  # return 0 since bash uses last test return code
-  return 0
 }
+
 run_commands() {
   time $@
 
@@ -99,9 +109,19 @@ EOF_HEADER
 parse_options() {
   # exit if no options left
   [[ -z $1 ]] && return 0
+  log "parse \$1: $1" 2>&$fddebug
 
   local do_shift=0
   case $1 in
+      -d|--debug)
+        enable_debug=1
+        ;;
+      -v|--verbose)
+	enable_verbose=1
+	;;
+      -q|--quiet)
+        enable_quiet=1
+        ;;
       -l|--log)
         enable_logging=1
         ;;
@@ -123,12 +143,12 @@ parse_options() {
       -p|--print-process)
         print_process=1
         ;;
-      -q|--quiet)
-        exec 1>/dev/null
-        ;;
       -h|--help)
         usage
         exit 0
+        ;;
+      --)
+        do_shift=3
         ;;
       *)
         do_shift=1
@@ -138,6 +158,11 @@ parse_options() {
     commands+=("$1")
   elif(($do_shift == 2)) ; then
     shift
+  elif (($do_shift == 3)) ; then
+    # got --, use all arguments left as options for other commands
+    shift
+    commands+=("$@")
+    return
   fi
   shift
   parse_options "$@"
@@ -159,12 +184,92 @@ cleanup() {
   [[ -f $logfile ]] && rm "$logfile"
 }
 
-signal_handler() {
-  trap - SIGINT SIGTERM
-  cleanup
-  local active_jobs=$(jobs -p)
-  [[ -n $active_jobs ]] && kill -SIGINT $active_jobs
-  error_exit 1 "Signal received: stopping backup" 
+prepare_env() {
+  set_descriptors
 }
 
+prepare() {
+  export PATH_USER_LIB=${PATH_USER_LIB:-"$HOME/.local/lib/"}
+
+  set -e
+  source_libs
+  set +e
+
+  set_descriptors
+}
+
+source_libs() {
+  source "${PATH_USER_LIB}libutils.sh"
+  source "${PATH_USER_LIB}libcolors.sh"
+}
+
+set_descriptors() {
+  if (($enable_verbose)); then
+    exec {fdverbose}>&2
+  else
+    exec {fdverbose}>/dev/null
+  fi
+  if (($enable_debug)); then
+    set -xv
+    exec {fddebug}>&2
+  else
+    exec {fddebug}>/dev/null
+  fi
+}
+
+set_signal_handlers() {
+  trap sigh_abort SIGABRT
+  trap sigh_alarm SIGALRM
+  trap sigh_hup SIGHUP
+  trap sigh_cont SIGCONT
+  trap sigh_usr1 SIGUSR1
+  trap sigh_usr2 SIGUSR2
+  trap sigh_cleanup SIGINT SIGQUIT SIGTERM EXIT
+}
+
+unset_signal_handlers() {
+  trap - SIGABRT
+  trap - SIGALRM
+  trap - SIGHUP
+  trap - SIGCONT
+  trap - SIGUSR1
+  trap - SIGUSR2
+  trap - SIGINT SIGQUIT SIGTERM EXIT
+}
+sigh_abort() {
+  trap - SIGABRT
+}
+
+sigh_alarm() {
+  trap - SIGALRM
+}
+
+sigh_hup() {
+  trap - SIGHUP
+}
+
+sigh_cont() {
+  trap - SIGCONT
+}
+
+sigh_usr1() {
+  trap - SIGUSR1
+}
+
+sigh_usr2() {
+  trap - SIGUSR2
+}
+
+sigh_cleanup() {
+  trap - SIGINT SIGQUIT SIGTERM EXIT
+  local active_jobs=$(jobs -p)
+  for p in $active_jobs; do
+    if [[ -e "/proc/$p" ]]; then
+      kill "$p" >/dev/null 2>&1
+      wait "$p"
+    fi
+  done
+}
+
+prepare
 main "$@"
