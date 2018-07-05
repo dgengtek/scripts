84dd8e3feda27b58720cc76bde6532a47bc10211
dgengtek <dgengtek@users.noreply.github.com>
2018-07-05 22:47:30 +0200

    Refactor script install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 # Install scripts
+export PATH="$PATH:$HOME/.local/bin/:$HOME/.bin/"
+export PATH_USER_LIB="$HOME/.local/lib/"
 
 usage() {
   cat >&2 << EOF
@@ -11,62 +13,200 @@ EOF
 }
 
 main() {
-  local -r optlist="abcdefgh"
-  while getopts $optlist opt; do
-    case $opt in
-      a)
-	;;
-      b)
-	;;
-      *)
-	usage
-	;;
-    esac
-  done
-  shift $((OPTIND - 1))
+  # flags
+  local -i enable_verbose=0
+  local -i enable_quiet=0
+  local -i enable_debug=0
 
-
-  trap cleanup SIGINT SIGTERM EXIT
+  local -a options
+  local -a args
   local installer="tools/install.py"
-  ! command -v "$installer" && installer="install.py"
-  ! command -v "$installer" && error "Could not find $installer in PATH." && exit 1
-  prepare
-
   local setup_config="setup.ini"
-  [[ -n $1 ]] && setup_config=$1
 
-  python3 "$installer" "$setup_config" || exit 1
-  trap - SIGINT SIGTERM EXIT
+  check_dependencies
+  # parse input args 
+  parse_options "$@"
+  # set leftover options parsed local input args
+  set -- ${args[@]}
+  # remove args array
+  unset -v args
+  check_input_args "$@"
+
+  set_signal_handlers
+  prepare_env
+  pre_run
+  run "$@"
+  post_run 
+  unset_signal_handlers
+}
+
+
+################################################################################
+# script internal execution functions
+################################################################################
+
+run() {
+  local command=$1
+  if [[ $(type -t _${command}) == "function" ]]; then
+    echo ran $command
+    _${command}
+  else
+    python3 "$installer" "$setup_config" || exit 1
+  fi
 }
 
 check_dependencies() {
   if ! hash stow 2>&1 | logger -t install -p user.info; then
     error "'stow' is required for installation."
   fi
+  ! command -v "$installer" && installer="install.py"
+  ! command -v "$installer" && error "Could not find $installer in PATH." && exit 1
 }
 
-cleanup() {
-  trap - SIGINT SIGTERM EXIT
-  pushd "$HOME"
-  rmdir -p .local/{bin,lib}
-  popd
-  exit 1
+check_input_args() {
+  :
 }
 
-prepare() {
+prepare_env() {
+  set_descriptors
+  [[ -n $1 ]] && setup_config=$1
   pushd "$HOME"
   mkdir -p .local/{bin,lib}
   popd
+}
+
+prepare() {
+  export PATH_USER_LIB=${PATH_USER_LIB:-"$HOME/.local/lib/"}
+  local parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+  cd "$parent_path"
+}
+
+set_descriptors() {
+  if (($enable_verbose)); then
+    exec {fdverbose}>&2
+  else
+    exec {fdverbose}>/dev/null
+  fi
+  if (($enable_debug)); then
+    set -xv
+    exec {fddebug}>&2
+  else
+    exec {fddebug}>/dev/null
+  fi
+}
+
+set_signal_handlers() {
+  trap sigh_abort SIGABRT
+  trap sigh_alarm SIGALRM
+  trap sigh_hup SIGHUP
+  trap sigh_cont SIGCONT
+  trap sigh_usr1 SIGUSR1
+  trap sigh_usr2 SIGUSR2
+  trap sigh_cleanup SIGINT SIGQUIT SIGTERM EXIT
+}
+
+unset_signal_handlers() {
+  trap - SIGABRT
+  trap - SIGALRM
+  trap - SIGHUP
+  trap - SIGCONT
+  trap - SIGUSR1
+  trap - SIGUSR2
+  trap - SIGINT SIGQUIT SIGTERM EXIT
+}
+
+pre_run() {
+  :
+}
+post_run() {
+  :
+}
+
+parse_options() {
+  # exit if no options left
+  [[ -z $1 ]] && return 0
+  log "parse \$1: $1" 2>&$fddebug
+
+  local do_shift=0
+  case $1 in
+      -d|--debug)
+        enable_debug=1
+        ;;
+      -v|--verbose)
+	enable_verbose=1
+	;;
+      -q|--quiet)
+        enable_quiet=1
+        ;;
+      -h|--help)
+        usage
+        exit 0
+        ;;
+      -p|--path)
+        path=$2
+        do_shift=2
+        ;;
+      --)
+        do_shift=3
+        ;;
+      *)
+        do_shift=1
+	;;
+  esac
+  if (($do_shift == 1)) ; then
+    args+=("$1")
+  elif (($do_shift == 2)) ; then
+    # got option with argument
+    shift
+  elif (($do_shift == 3)) ; then
+    # got --, use all arguments left as options for other commands
+    shift
+    options+=("$@")
+    return
+  fi
+  shift
+  parse_options "$@"
+}
+
+sigh_abort() {
+  trap - SIGABRT
+}
+
+sigh_alarm() {
+  trap - SIGALRM
+}
+
+sigh_hup() {
+  trap - SIGHUP
+}
+
+sigh_cont() {
+  trap - SIGCONT
+}
+
+sigh_usr1() {
+  trap - SIGUSR1
+}
+
+sigh_usr2() {
+  trap - SIGUSR2
+}
+
+sigh_cleanup() {
+  trap - SIGINT SIGQUIT SIGTERM EXIT
+  local active_jobs=$(jobs -p)
+  for p in $active_jobs; do
+    if [[ -e "/proc/$p" ]]; then
+      kill "$p" >/dev/null 2>&1
+      wait "$p"
+    fi
+  done
 }
 
 mkdir() {
   ! [[ -e $1 ]] && command mkdir "$@"
 }
 
-export PATH="$PATH:$HOME/.local/bin/:$HOME/.bin/"
-export PATH_USER_LIB="$HOME/.local/lib/"
-
-source ./lib/libutils.sh || source "${PATH_USER_LIB}libutils.sh"
 if [[ $(type -t error) != "function" ]]; then
 echo() ( 
   IFS=" " 
@@ -76,7 +216,5 @@ out() { echo "$1 $2" "${@:3}"; }
 error() { out "==> ERROR:" "$@"; } >&2
 fi
 
-# silence output
-exec > /dev/null
-
+prepare
 main "$@"
