8d5644b4303a30e04d68680653576cdc54645e17
dgengtek <dgengtek@users.noreply.github.com>
2018-10-31 23:33:54 +0100

    Add script for adding pipelines

---
+++ integration/concourse_add_pipeline.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import configparser
+import os
+import sys
+import subprocess
+
+METADATA = "metadata-concourse.ini"
+CONCOURSE_PIPELINE = "concourse.yml"
+
+
+def main():
+    global METADATA
+    global CONCOURSE_PIPELINE
+
+    config = configparser.ConfigParser()
+    relative_path = os.path.dirname(sys.argv[0])
+    if relative_path:
+        os.chdir(relative_path)
+    with open(METADATA) as f:
+        config.read_file(f)
+    post_receive = config["post-receive"]
+    pipeline_name = post_receive["pipeline_name"]
+    target = post_receive["target"]
+
+    concourse_fly = ["fly", "-t"]
+    concourse_fly.append(target)
+
+    run(*concourse_fly,
+        "set-pipeline",
+        "-p",
+        pipeline_name,
+        "-c",
+        CONCOURSE_PIPELINE)
+    run(*concourse_fly,
+        "unpause-pipeline",
+        "-p",
+        pipeline_name)
+
+
+def run(*args):
+    return subprocess.run(args)
+
+
+if __name__ == "__main__":
+    main()
