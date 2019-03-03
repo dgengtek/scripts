30627a25b444ba6634e9068bfb91532fb3409cf7
dgengtek <dgengtek@users.noreply.github.com>
2019-03-03 12:49:44 +0100

    Update startup

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 exec 2>&1
 exec 1>/dev/null
-run.sh firefox
-#run.sh keepassx 
-run.sh -q urxvtc -e 'tmuxp load ~/.tmuxp/wiki.yaml'
-run.sh -q urxvtc -e 'tmux new -s mutt'
-run.sh -q urxvtc -e 'tmuxp load ~/.tmuxp/irc.yaml'
+
+main() {
+  if ! hash run.sh; then
+    echo "Could not find run.sh in path." >&2
+    exit 1
+  fi
+
+  # decrypt smartcard first
+  echo | gpg -d
+  run.sh -n -q -- firefox
+  #run.sh keepassx 
+  run.sh -n -q -- urxvtc -e 'tmuxp load ~/.tmuxp/wiki.yaml'
+  run.sh -n -q -- urxvtc -e 'tmux new -s mutt'
+  run.sh -n -q -- urxvtc -e 'tmuxp load ~/.tmuxp/irc.yaml'
+  run.sh -n -q -- urxvtc -e 'tmux new -s salt'
+  run.sh -n -q -- urxvtc -e 'mosh baha'
+
+  tmux send-keys -t mutt 'mutt' ENTER
+  tmux send-keys -t salt 'lxc exec salt bash' ENTER
+}
+
+main "$@"
