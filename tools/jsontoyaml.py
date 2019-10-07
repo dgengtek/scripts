ab98656668cdd4cb24444c924c6dab6963232640
dgengtek <dgengtek@users.noreply.github.com>
2019-10-07 19:15:27 +0200

    Add tools to convert data formats

---
+++ tools/jsontoyaml.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import yaml
+import json
+data_input = ""
+if len(sys.argv) == 1:
+    data_input = sys.stdin
+else:
+    data_input = open(sys.argv[1])
+
+data = json.load(data_input)
+print(yaml.dump(data))
---
+++ tools/yamltojson.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import yaml
+import json
+data_input = ""
+if len(sys.argv) == 1:
+    data_input = sys.stdin
+else:
+    data_input = open(sys.argv[1])
+
+data = yaml.safe_load(data_input)
+json.dump(data, sys.stdout)
