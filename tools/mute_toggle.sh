ba5d07e0f62bb166e24d3ca3b8a7d0a1ec1dde51
dgengtek <dgengtek@users.noreply.github.com>
2020-01-19 15:04:05 +0100

    Toggle mute all indexes

--- tools/mute_toggle.sh
+++ tools/mute_toggle.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+
 main() {
   if ! hash pacmd 2>/dev/null; then
     echo asexit
@@ -8,26 +10,31 @@ main() {
   pulse_toggle
 }
 
+
 pulse_toggle() {
   local output=$(pacmd list-sinks)
-  local index=$(echo "$output"|grep "* index"|awk -F':' '{print $2}')
+  local indexes=$(echo "$output"|grep "index"|awk -F':' '{print $2}')
 
-  if [[ -z $index ]]; then
-    echo "no index matched"
-    exit 1
-  fi
-  local answer=$(echo "$output"|grep -A 20 "* index"|grep mute|awk '{print $2}')
-  if [[ -z $answer ]]; then
-    echo "no match in answer"
-    exit 1
-  elif [[ $answer == "yes" ]]; then
-    answer="no"
-  else
-    answer="yes"
-  fi
-  pacmd set-sink-mute $index $answer
+  while read -d $'\n' index; do
+    if [[ -z $index ]]; then
+      echo "no index matched" >&2
+      continue
+    fi
+    local answer=$(echo "$output"|grep -A 20 "index: $index"|awk '/mute/ {print $2}')
+    if [[ -z $answer ]]; then
+      echo "$index: no match in answer" >&2
+      continue
+    elif [[ $answer == "yes" ]]; then
+      answer="no"
+    else
+      answer="yes"
+    fi
+    pacmd set-sink-mute $index $answer
+  done < <(echo "$indexes")
 
 }
+
+
 select_index() {
   local index=$1
   local -i valid_selection=0
@@ -45,6 +52,8 @@ select_index() {
     fi
   done
 }
+
+
 alsa_toggle() {
   amixer -D hw:0 -s -q << EOF
 sset Master toggle 
@@ -52,5 +61,6 @@ sset Headphone toggle
 sset Front toggle 
 EOF
 }
+
 
 main
