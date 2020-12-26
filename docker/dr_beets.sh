8e856158f7006906019306843900209cd137da8d
dgengtek <dgengtek@users.noreply.github.com>
2020-12-26 20:57:12 +0100

    Add beets script for importing with plugins

---
+++ docker/dr_beets.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+readonly config_plugins="/tmp/config_beets_plugins.yaml"
+cat > "$config_plugins" << EOF
+plugins:
+  - chroma
+  - lastgenre
+  - convert
+EOF
+
+sudo docker run \
+  --rm \
+  -it \
+  --name "beets" \
+  --mount type=bind\
+,source="$HOME/.config/beets/config.yaml"\
+,destination=/home/beetu/.config/beets/config.yaml,readonly \
+  --mount type=bind\
+,source="$config_plugins"\
+,destination=/home/beetu/.config/beets/plugins_config.yaml,readonly \
+  --mount type=bind\
+,source=/mnt/nfs/homes/dgeng/music\
+,destination=/oldmusic,readonly \
+  --mount type=bind\
+,source=/mnt/hdd/bigX/unsorted/beets\
+,destination=/mnt/hdd/bigX/unsorted/beets\
+  beets --config "/home/beetu/.config/beets/plugins_config.yaml" "$@"
