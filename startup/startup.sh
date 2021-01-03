8eb179ff2bcf7c1536475e1f668ff009c76f6409
dgengtek <dgengtek@users.noreply.github.com>
2021-01-03 16:06:53 +0100

    Replace with tmuxp sessions to startup

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-exec 2>&1
-exec 1>/dev/null
+# exec 2>&1
+# exec 1>/dev/null
 
 main() {
   if ! hash run.sh; then
@@ -8,6 +8,7 @@ main() {
     exit 1
   fi
 
+  # TODO: 144hz not working out of box with x11 configuration?
   xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144 --left-of DVI-D-0 --primary;
   xrandr --output DVI-D-0 --mode 1920x1080 --rate 60 --pos 1920x0
 
@@ -17,26 +18,27 @@ main() {
   run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/wiki.yaml'
   run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/private.yaml'
   run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/monitor.yaml'
-  run.sh -n -- alacritty -e 'tmux new -s mutt'
+  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/mail.yaml'
   # run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/irc.yaml'
-  run.sh -n -- alacritty -e 'tmux new -s ci'
+  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/ci.yaml'
   run.sh -n -- alacritty -e 'tmux new -s admin'
   run.sh -n -- alacritty -e 'tmux new -s run'
 
-  # wait until tmux server is up
+  # wait until tmux server is up and sessions are running
   while ! tmux has-session >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t run >/dev/null 2>&1; do sleep 1; done
-  while ! tmux has-session -t mutt >/dev/null 2>&1; do sleep 1; done
+  while ! tmux has-session -t mail >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t ci >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t private >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t monitor >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t admin >/dev/null 2>&1; do sleep 1; done
 
-  tmux send-keys -t mutt 'mutt' ENTER
-  tmux send-keys -t ci 'fly -t intranet login && watch fly -t intranet builds' ENTER
-  # TODO: wait for pinentry to successfully close
   # run.sh -n -q -- urxvt -e 'ssh baha'
-  # run.sh -n -q -- alacritty -e 'mosh -p 60000 baha'
+  # wait for gpg2 to close in other session
+  while ! [[ -z "$(pgrep gpg2)" ]]; do
+    sleep 1
+  done
+  run.sh -n -q -- alacritty -e 'mosh -p 60000 baha'
 }
 
 main "$@"
