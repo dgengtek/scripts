cdec24f06048e4178eb987881f72c4bb73661563
dgengtek <dgengtek@users.noreply.github.com>
2021-06-23 15:32:44 +0200

    Use exec

--- startup/startup.sh
+++ startup/startup.sh
@@ -15,15 +15,15 @@ main() {
   xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144 --left-of DVI-D-0 --primary;
   xrandr --output DVI-D-0 --mode 1920x1080 --rate 60 --pos 1920x0
 
-  run.sh -n -- firefox
-  run.sh -n -- freeplane
-  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/wiki.yaml'
-  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/private.yaml'
-  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/mail.yaml'
-  # run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/irc.yaml'
-  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/ci.yaml'
-  run.sh -n -- alacritty -e 'tmuxp load ~/.tmuxp/run.yaml'
-  run.sh -n -- alacritty -e 'tmux new -s admin'
+  run.sh -e -- firefox
+  run.sh -e -- freeplane
+  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/wiki.yaml'
+  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/private.yaml'
+  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/mail.yaml'
+  # run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/irc.yaml'
+  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/ci.yaml'
+  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/run.yaml'
+  run.sh -e -- alacritty -e 'tmux new -s admin'
 
   # wait until tmux server is up and sessions are running
   while ! tmux has-session >/dev/null 2>&1; do sleep 1; done
@@ -34,14 +34,14 @@ main() {
   while ! tmux has-session -t admin >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t wiki >/dev/null 2>&1; do sleep 1; done
 
-  # run.sh -n -q -- urxvt -e 'ssh baha'
+  # run.sh -e -- urxvt -e 'ssh baha'
   sleep 10 # wait for startup of other sessions to create lock file
   # wait for lock to release
   while [[ -d "$STARTUP_GPG_LOCK" ]]; do
     echo "waiting for lock($STARTUP_GPG_LOCK) to release" >&2
     sleep 1
   done
-  run.sh -n -q -- alacritty -e 'mosh -p 60000 baha'
+  run.sh -e -- alacritty -e 'mosh -p 60000 baha'
   systemctl --user start redshift dunst
 }
 
