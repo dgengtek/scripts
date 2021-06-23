4e9fa66e22f110c7dce62c0fc4d20796ca4c7eb9
dgengtek <dgengtek@users.noreply.github.com>
2021-06-23 21:57:51 +0200

    Create in workspaces

--- startup/startup.sh
+++ startup/startup.sh
@@ -15,24 +15,26 @@ main() {
   xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144 --left-of DVI-D-0 --primary;
   xrandr --output DVI-D-0 --mode 1920x1080 --rate 60 --pos 1920x0
 
-  run.sh -e -- firefox
   run.sh -e -- freeplane
-  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/wiki.yaml'
-  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/private.yaml'
-  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/mail.yaml'
-  # run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/irc.yaml'
-  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/ci.yaml'
-  run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/run.yaml'
   run.sh -e -- alacritty -e 'tmux new -s admin'
+  i3-msg 'workspace "2:surf"; exec firefox'
+  i3-msg 'workspace "3:priv"; exec alacritty -e bash -c "tmuxp load $HOME/.tmuxp/private.yaml"'
+  # run.sh -e -- alacritty -e 'tmuxp load ~/.tmuxp/irc.yaml'
+
+  # move all to container and scratchpad later
+  i3-msg 'workspace "8:0"; exec alacritty -e bash -c "tmuxp load $HOME/.tmuxp/ci.yaml"'
+  i3-msg 'workspace "8:0"; exec alacritty -e bash -c "tmuxp load $HOME/.tmuxp/run.yaml"'
+  i3-msg 'workspace "8:0"; exec alacritty -e bash -c "tmuxp load $HOME/.tmuxp/scratchpad.yaml"'
+  i3-msg 'workspace "8:0"; exec alacritty -e bash -c "tmuxp load $HOME/.tmuxp/wiki.yaml"'
 
   # wait until tmux server is up and sessions are running
   while ! tmux has-session >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t run >/dev/null 2>&1; do sleep 1; done
-  while ! tmux has-session -t mail >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t ci >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t private >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t admin >/dev/null 2>&1; do sleep 1; done
   while ! tmux has-session -t wiki >/dev/null 2>&1; do sleep 1; done
+  while ! tmux has-session -t scratchpad >/dev/null 2>&1; do sleep 1; done
 
   # run.sh -e -- urxvt -e 'ssh baha'
   sleep 10 # wait for startup of other sessions to create lock file
@@ -43,6 +45,8 @@ main() {
   done
   run.sh -e -- alacritty -e 'mosh -p 60000 baha'
   systemctl --user start redshift dunst
+  i3-msg 'workspace "8:0"; focus parent; move scratchpad'
+  i3-msg 'workspace "1:shells"'
 }
 
 main "$@"
