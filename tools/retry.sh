5da9bfda5e2b739120d412f88158cb6a4746584b
dgengtek <dgengtek@users.noreply.github.com>
2021-11-05 14:38:21 +0100

    Add retry

---
+++ tools/retry.sh
@@ -0,0 +1,16 @@
+main() {
+  local -r n_times=${1:-3}
+  local -r interval=${2:-3}
+  shift 2
+  trap "trap - SIGINT;exit 128;" SIGINT
+  let count=1
+  while ! $@; do
+    if (($count == 3)); then
+      break
+    fi
+    sleep $interval
+    let count+=1
+  done
+  trap - SIGINT
+}
+main "$@"
