5b71561441a3776959f54601ceabbb918a511944
dgengtek <dgengtek@users.noreply.github.com>
2022-07-11 22:12:17 +0200

    Prompt for input formatting

--- tools/prompt_run.py
+++ tools/prompt_run.py
@@ -11,33 +11,134 @@ import signal
 import subprocess
 import os
 import shlex
+import re
 logger = logging.getLogger(__name__)
 
 
 def main():
-    """
-    main method
-    """
-
     atexit.register(cleanup, "Cleanup before exit")
     signal.signal(signal.SIGALRM, handler)
 
-    args = parse_args().args
+    args = parse_args()
+    print(args)
+    enable_formatting = args.format
+    args = args.args
     if not args:
         die("Arguments for the prefix to execute are required")
 
-    while True:
-        try:
-            user_input = input("{} > ".format(" ".join(args)))
-        except EOFError:
-            sys.exit(0)
-        except KeyboardInterrupt:
-            print()
-            continue
+    if enable_formatting:
+        template = TemplatePrompt.check_valid(list(args))
 
-        call_args = list(args)
-        call_args.extend(shlex.split(user_input))
+    while True:
+        if enable_formatting:
+            call_args = template.get_call_args()
+        else:
+            call_args = list(args)
+            user_input = simple_prompt(" ".join(args))
+            if user_input is True:
+                continue
+            call_args.extend(shlex.split(user_input))
+
         subprocess.call(call_args)
+
+
+class TemplatePrompt():
+    def __init__(self, pattern, template_strings):
+        self.pattern = pattern
+        self.template_strings = list(template_strings)
+        self.template_string = " ".join(self.template_strings)
+        self.result = self.findall()
+        self.count = len(self.result)
+
+    def prompt(self):
+        raise NotImplementedError()
+
+    def findall(self):
+        return re.findall(self.pattern, self.template_string)
+
+    def is_valid(self):
+        return self.count > 0
+
+    def format(self, user_data):
+        raise NotImplementedError()
+
+    def get_call_args(self):
+        prerr(self.template_string)
+        user_data = self.prompt()
+        result = self.format(user_data)
+        return shlex.split(result)
+
+    @classmethod
+    def check_valid(cls, template_strings):
+        template_pos = TemplatePromptPos(template_strings)
+        template_keys = TemplatePromptKeys(template_strings)
+
+        pos_valid = template_pos.is_valid()
+        keys_valid = template_keys.is_valid()
+        if (pos_valid and keys_valid) or (not pos_valid and not keys_valid):
+            raise FormatException("Unable to proceed with given template string. Keys: {}, Pos: {}".format(template_keys.count, template_pos.count))
+
+        template = template_pos
+        if template.is_valid():
+            return template
+        template = template_keys
+        if template.is_valid():
+            return template
+
+
+class TemplatePromptPos(TemplatePrompt):
+    def __init__(self, template_strings):
+        pattern = re.compile(r"{}")
+        super().__init__(pattern, template_strings)
+
+    def prompt(self):
+        data = []
+        count = list(reversed(range(self.count)))
+        while count:
+            i = count.pop()
+            user_input = simple_prompt("Pos {}".format(i))
+            if not user_input:
+                prerr("Input for {} is empty. Repeat".format(i))
+                count.append(i)
+                continue
+            data.append(user_input)
+        return data
+
+    def format(self, user_data):
+        return self.template_string.format(*user_data)
+
+
+class TemplatePromptKeys(TemplatePrompt):
+    def __init__(self, template_strings):
+        pattern = re.compile(r"{(.[^{}]*)}")
+        super().__init__(pattern, template_strings)
+
+    def prompt(self):
+        data = {}
+        count = list(reversed(self.result))
+        while count:
+            k = count.pop()
+            user_input = simple_prompt("Key {}".format(k))
+            if not user_input:
+                prerr("Input for {} is empty. Repeat".format(k))
+                count.append(k)
+                continue
+            data.update({k: user_input})
+        return data
+
+    def format(self, user_data):
+        return self.template_string.format(**user_data)
+
+
+def simple_prompt(prompt_prefix):
+    try:
+        user_input = input("{} > ".format(prompt_prefix))
+    except EOFError:
+        sys.exit(0)
+    except KeyboardInterrupt:
+        print()
+        return True
+    return user_input
 
 
 def usage():
@@ -59,6 +160,11 @@ def parse_args():
     parser.add_argument(
         'args',
         nargs=argparse.REMAINDER)
+
+    parser.add_argument(
+            "-f", "--format",
+            help="format the input string either with key=value key2=value or positional with space delimiter $1 $2 provided in the prompt",
+            action="store_true")
 
     return parser.parse_args()
 
@@ -90,6 +196,10 @@ def check_env(var):
     else:
         return True
     return False
+
+
+class FormatException(Exception):
+    pass
 
 
 if __name__ == "__main__":
