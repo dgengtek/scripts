d4d17d291eb7d37e8fcb57e5da2e441740bd9bf7
dgengtek <dgengtek@users.noreply.github.com>
2024-07-17 01:08:34 +0200

    Configure nix install

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { config = { }; overlays = [ ]; inherit system; };
+      lib = pkgs.callPackage ./lib.nix { inherit poetry2nix; };
+    in
+    {
+      packages.${system}.default = lib.scripts;
+      inherit lib;
+    };
+}
