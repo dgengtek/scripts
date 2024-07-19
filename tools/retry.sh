493bdac94676572c3a2f8cda171f0c5ea65b1dfe
dgengtek <dgengtek@users.noreply.github.com>
2024-07-19 20:29:01 +0200

    Add missing shebangs

--- tools/retry.sh
+++ tools/retry.sh
@@ -1,16 +1,18 @@
+#!/usr/bin/env bash
+
 main() {
-  local -r n_times=${1:-3}
-  local -r interval=${2:-3}
-  shift 2
-  trap "trap - SIGINT;exit 128;" SIGINT
-  let count=1
-  while ! $@; do
-    if (($count == 3)); then
-      break
-    fi
-    sleep $interval
-    let count+=1
-  done
-  trap - SIGINT
+	local -r n_times=${1:-3}
+	local -r interval=${2:-3}
+	shift 2
+	trap "trap - SIGINT;exit 128;" SIGINT
+	let count=1
+	while ! $@; do
+		if (($count == 3)); then
+			break
+		fi
+		sleep $interval
+		let count+=1
+	done
+	trap - SIGINT
 }
 main "$@"
